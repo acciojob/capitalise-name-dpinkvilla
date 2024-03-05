89975d87522b1374fce6480203fe031c4de4dd22
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-05 11:16:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let inputfield=document.getElementById("fname");
+
+function over(){
+	inputfield.value= inputfield.value.toUpperCase();
+}
+
+inputfield.addEventListener("blur", over);
